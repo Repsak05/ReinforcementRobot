c3a3c05097a708881c9792fa2e93a234024b0a76
KasperL22R <119696654+Repsak05@users.noreply.github.com>
2025-03-28 09:34:28 +0100

    Interaction: Encoder to number

--- Interaktion/makeInteractionWork/makeInteractionWork.ino
+++ Interaktion/makeInteractionWork/makeInteractionWork.ino
@@ -1,11 +1,17 @@
 #include <Wire.h>
 #include "rgb_lcd.h"
+#include <Encoder.h>
+
 rgb_lcd lcd;
 
 const int MIN_POSITION = 40;
 const int MAX_POSITION = 240;
 
-#define ROTARY_ANGLE_SENSOR A1
+Encoder knobRight(2, 3);
+long positionRight = 0;
+
+
+#define ROTARY_ANGLE_SENSOR A1  
 
 int convertBetweenPositions(int current, int MIN, int MAX){
   // if(MAX_POSITION > MIN_POSITION) swap(MIN_POSITION, MAX_POSITION);
@@ -14,8 +20,6 @@ int convertBetweenPositions(int current, int MIN, int MAX){
   float end = 1023;
   return MIN + (abs(current / (start - end)) * (MAX - MIN));
 }
-
-
 
 void setup() {
   Serial.begin(115200);
@@ -32,25 +36,30 @@ void writeOnLCD(String s, int val, int col, int row){
   lcd.print(val);
 }
 
+int getEncoderPosition(){
+  long newRight;
+  newRight = knobRight.read();
+  if (newRight != positionRight) {
+    Serial.println();
+    positionRight = newRight;
+  }
+  return int(newRight / 4);
+}
+
 void loop(){
+  Serial.println(getEncoderPosition(););
+
   lcd.clear();
 
   // Sliding sensor
   int sensorValue = analogRead(A0);
-  // Serial.print("Sensor: ");
-  // Serial.println(convertBetweenPositions(sensorValue, MIN_POSITION, MAX_POSITION));
-
 
   //Encoder
   int encoderValue = analogRead(ROTARY_ANGLE_SENSOR);
-  // // Serial.print("Encoder: ");
-  // // Serial.println(convertBetweenPositions(encoderValue, 0, 300));
- 
  
   // LCD
   writeOnLCD("Episoder", convertBetweenPositions(encoderValue, 0, 300), 0, 0);
   writeOnLCD("Placering", convertBetweenPositions(sensorValue, MIN_POSITION, MAX_POSITION), 0, 1);
-  // lcd.print("Episoder:" + convertBetweenPositions(encoderValue, 0, 300));
 
   delay(100);
 
